7314bc6f8ad56c23a917c8ff65cae18e2e5669b6
Prem Kumar <prembammidi25@msitprogram.net>
2020-06-30 11:54:29 +0530

    ginger tool template

--- Django Website/django_website/blog/views.py
+++ Django Website/django_website/blog/views.py
@@ -1,15 +1,37 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+# from django.http import HttpResponse
 
 # Create your views here.
 # def home(request):
 #     return HttpResponse('<h1>Blog Home</h1>')
 
 
-def about(request):
-    return HttpResponse('<h1>Blog About</h1>')
+# def about(request):
+#     return HttpResponse('<h1>Blog About</h1>')
+
+posts = [
+    {
+        'author': 'Prem',
+        'title': 'Blog Post 1',
+        'content': 'First post content',
+        'date_posted': 'June 30, 2020'
+    },
+    {
+        'author': 'PremRev',
+        'title': 'Blog Post 2',
+        'content': 'Second post content',
+        'date_posted': 'July 1, 2020'
+    },  
+]
 
 def home(request):
-    return render(request, 'blog/home.html')
+    context = {
+        'posts': posts
+    }
+    return render(request, 'blog/home.html', context)
+
+def about(request):
+    return render(request, 'blog/about.html')
+
 
 
