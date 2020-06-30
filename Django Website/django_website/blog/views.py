641100c3428fd139e2f20d3a11bfd80c467bb75e
Prem Kumar <prembammidi25@msitprogram.net>
2020-06-30 10:16:50 +0530

    template render

--- Django Website/django_website/blog/views.py
+++ Django Website/django_website/blog/views.py
@@ -2,10 +2,14 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def home(request):
-    return HttpResponse('<h1>Blog Home</h1>')
+# def home(request):
+#     return HttpResponse('<h1>Blog Home</h1>')
+
 
 def about(request):
     return HttpResponse('<h1>Blog About</h1>')
+
+def home(request):
+    return render(request, 'blog/home.html')
 
 
